d5c8030a34676ea36661ec4495c1dbd48aa938a2
GongasSantos15 <goncalo.c.r.santos@protonmail.com>
2022-01-25 09:32:54 +0000

    Changed codigoJogo

---
+++ codigoJogo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GameOffline from './components/GameOffline';
+
+function App() {
+  return (
+    <div className="App">
+      <GameOffline />
+    </div>
+  );
+}
+
+export default App;
---
+++ login/src/components/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function login() {
+  return (
+  <div>
+    <h1>Rock, Paper, Scissors</h1>
+    <button></button>
+    <button></button>
+    <button></button>
+  </div>
+  );
+}
